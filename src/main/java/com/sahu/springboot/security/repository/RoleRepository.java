a02bf3a5dfe6ae1bd6a7a30dbc9493fc08aebbbd
Nirmala Kumar Sahu <nirmalakumarsahu7@gmail.com>
2025-08-18 23:04:38 +0530

    repositories

---
+++ src/main/java/com/sahu/springboot/security/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.sahu.springboot.security.repository;
+
+import com.sahu.springboot.security.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
