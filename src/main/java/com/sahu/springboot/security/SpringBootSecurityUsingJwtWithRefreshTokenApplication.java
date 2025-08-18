c27291a5e99b0b325330d8273728c9628e421518
Nirmala Kumar Sahu <nirmalakumarsahu7@gmail.com>
2025-08-18 23:03:08 +0530

    initial project

---
+++ src/main/java/com/sahu/springboot/security/SpringBootSecurityUsingJwtWithRefreshTokenApplication.java
@@ -0,0 +1,13 @@
+package com.sahu.springboot.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityUsingJwtWithRefreshTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityUsingJwtWithRefreshTokenApplication.class, args);
+	}
+
+}
