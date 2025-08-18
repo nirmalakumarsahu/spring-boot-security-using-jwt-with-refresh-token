cc2748474c54a4be1a255552b7fd4b6948d73b83
Nirmala Kumar Sahu <nirmalakumarsahu7@gmail.com>
2025-08-19 01:01:21 +0530

    Update RefreshTokenService.java

--- src/main/java/com/sahu/springboot/security/service/RefreshTokenService.java
+++ src/main/java/com/sahu/springboot/security/service/RefreshTokenService.java
@@ -1,7 +1,14 @@
 package com.sahu.springboot.security.service;
 
+import com.sahu.springboot.security.dto.RefreshTokenRequest;
 import com.sahu.springboot.security.model.RefreshToken;
+
+import java.util.Optional;
 
 public interface RefreshTokenService {
     RefreshToken createRefreshToken(String username);
+
+    Optional<RefreshToken> findByToken(String token);
+
+    RefreshToken verifyRefreshToken(RefreshToken refreshToken);
 }
