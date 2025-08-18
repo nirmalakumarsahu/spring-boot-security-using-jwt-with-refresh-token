6854ff6c50d7caa12792ba0ee3dfd96927c4ffab
Nirmala Kumar Sahu <nirmalakumarsahu7@gmail.com>
2025-08-18 23:03:35 +0530

    application yml and docker compose

---
+++ src/test/java/com/sahu/springboot/security/SpringBootSecurityUsingJwtWithRefreshTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sahu.springboot.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityUsingJwtWithRefreshTokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
